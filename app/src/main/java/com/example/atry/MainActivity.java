aa910373b8297796ba0395f2291a0143d6d2fc08
BernigaudQuentin <bernigaud@et.esiea.fr>
2020-05-10 18:21:13 +0200

    [Affichage][Liste][Recycler View] FIN du TD3

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.atry;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ListeAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }// define an adapter
+        mAdapter = new ListeAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+
     }
 }
