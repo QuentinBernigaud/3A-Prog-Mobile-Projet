658bb711376b2e73526f5bafa7172b877e9b83c1
BernigaudQuentin <bernigaud@et.esiea.fr>
2020-05-24 01:39:26 +0200

    [Liste][TD5][Cache][PokéApi] Ajout fonctionnel de la fonction saveList pour accéder à la liste en cache + centralisation des Constants json

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -11,8 +11,9 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
-import java.util.ArrayList;
+import java.lang.reflect.Type;
 import java.util.List;
 
 import retrofit2.Call;
@@ -41,11 +42,30 @@ public class MainActivity extends AppCompatActivity {
         gson = new GsonBuilder()
                 .setLenient()
                 .create();
+        List<Pokemon> pokemonList = getDataFromCache();
+        if(pokemonList != null){
+        showList(pokemonList);
+        } else {
 
-    makeApiCall();
+            makeApiCall();
+        }
+
     }
 
-        private void showList(List<Pokemon> pokemonList){
+    private List<Pokemon> getDataFromCache() {
+
+        String jsonPokemon = sharedPreferences.getString(Constants.KEY_POKEMON_LIST, null);
+
+        if(jsonPokemon == null) {
+            return null;
+        } else {
+            Type listType = new TypeToken<List<Pokemon>>(){}.getType();
+            return gson.fromJson(jsonPokemon, listType);
+        }
+
+    }
+
+    private void showList(List<Pokemon> pokemonList){
 
             recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
             recyclerView.setHasFixedSize(true);
@@ -96,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         String jsonString = gson.toJson(pokemonList);
         sharedPreferences
                 .edit()
-                .putString("jsonPokemonList", jsonString)
+                .putString("Constants.KEY_POKEMON_LIST", jsonString)
                 .apply();
 
         Toast.makeText(getApplicationContext(), "Liste Saved", Toast.LENGTH_SHORT).show();
